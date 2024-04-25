cdd11e72bfee8e49691772490dfbdc3f63c2541d
FastGlory <jubaredjradj@gmail.com>
2024-04-25 12:31:11 -0400

    Ajout getSet pour etudiant professeur

--- ProjetDiego/src/main/java/com/example/projetdiego/etudiant.java
+++ ProjetDiego/src/main/java/com/example/projetdiego/etudiant.java
@@ -2,4 +2,12 @@ package com.example.projetdiego;
 
 public class etudiant extends personne {
     private double CoteR;
+
+    public double getCoteR() {
+        return CoteR;
+    }
+
+    public void setCoteR(double coteR) {
+        CoteR = coteR;
+    }
 }
--- ProjetDiego/src/main/java/com/example/projetdiego/professeur.java
+++ ProjetDiego/src/main/java/com/example/projetdiego/professeur.java
@@ -3,4 +3,11 @@ package com.example.projetdiego;
 public class professeur extends personne{
     private double salaire;
 
+    public double getSalaire() {
+        return salaire;
+    }
+
+    public void setSalaire(double salaire) {
+        this.salaire = salaire;
+    }
 }

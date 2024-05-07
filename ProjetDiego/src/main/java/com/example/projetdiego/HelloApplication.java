c9b281ce4b1c4230391352c3f88119ef7eef69f7
Diego Fanara Alvarez <2262164@collegemv.qc.ca>
2024-05-07 11:03:50 -0400

    Corrextion Affichage

--- ProjetDiego/src/main/java/com/example/projetdiego/HelloApplication.java
+++ ProjetDiego/src/main/java/com/example/projetdiego/HelloApplication.java
@@ -10,14 +10,15 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("PageAccueil.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        stage.setTitle("Page Acceuil");
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
+
         launch();
     }
 }
